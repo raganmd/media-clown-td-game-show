079c9a48c40e109b0fa66d71509286b186fc7d8f
Matthew Ragan <raganmd@gmail.com>
2021-05-20 18:21:43 -0700

    OSC API Updates
    Updates to OSC commands
    list of commands, ranges, values, and outcomes can be found here:
    
    https://docs.google.com/spreadsheets/d/1xjI4_OMwWAgFkZPIuWZxEjdfsVjhhQT9r062u1ZMjjE/edit#gid=0

--- timer/dats/python/oscinActions.py
+++ timer/dats/python/oscinActions.py
@@ -2,11 +2,23 @@ def run_me(args):
 	print(args)
 	return
 
-def push2(args):
-	op.output.Start_timer()
+def timer_start(args):
+	op.output.Timer_start()
 
-def fader3(args):
+def timer_init(args):
+	op.output.Timer_init()
+
+def update_timer_speed(args):
 	op.output.Update_timer_speed(args[0])
 
-def toggle2(args):
+def update_timer_length(args):
+	op.output.Update_timer_length(args[0])
+
+def toggle_play(args):
 	op.output.Toggle_play(args[0])
+	
+def count_down(args):
+	op.output.Count_direction(args[0])
+	
+def count_up(args):
+	op.output.Count_direction(args[0])
--- timer/dats/python/outputEXT.py
+++ timer/dats/python/outputEXT.py
@@ -3,11 +3,22 @@ class Output:
 		self.My_op = my_op
 		print(f"Output init from {my_op}")
 	
-	def Start_timer(self):
+	def Timer_start(self):
 		ipar.Settings.Timerstart.pulse()
+
+	def Timer_init(self):
+		ipar.Settings.Timerinit.pulse()
 
 	def Update_timer_speed(self, val):
 		ipar.Settings.Timerspeed = val
 
 	def Toggle_play(self, state):
 		ipar.Settings.Timerplay = state
+		
+	def Update_timer_length(self, val):
+		ipar.Settings.Timerlength = val
+		ipar.Settings.Timerstart.pulse()
+		pass
+	
+	def Count_direction(self, direction):
+		ipar.Settings.Countdirection = direction[0]
