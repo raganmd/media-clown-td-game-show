c184b629cf3f8bb070a8202da698db739bcb3f54
Matthew Ragan <raganmd@gmail.com>
2021-06-10 23:10:41 -0700

    Updates to Show Files

--- perceval/dats/python/oscinActions.py
+++ perceval/dats/python/oscinActions.py
@@ -2,23 +2,14 @@ def run_me(args):
 	print(args)
 	return
 
-def timer_start(args):
-	op.output.Timer_start()
+def toggle_logo(args):
+	ipar.Settings.Logo = args[0]
 
-def timer_init(args):
-	op.output.Timer_init()
+def set_audio(args):
+	ipar.Settings.Audiolevel = args[0]
 
-def update_timer_speed(args):
-	op.output.Update_timer_speed(args[0])
+def toggle_glitch(args):
+	parent(2).Toggle_glitch(args[0])
 
-def update_timer_length(args):
-	op.output.Update_timer_length(args[0])
-
-def toggle_play(args):
-	op.output.Toggle_play(args[0])
-	
-def count_down(args):
-	op.output.Count_direction(args[0])
-
-def count_up(args):
-	op.output.Count_direction(args[0])
+def set_glitch(args):
+	ipar.Settings.Glitch = args[0]
