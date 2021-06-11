128164b53bbc593cecd758e339ada6b22f841a7f
Matthew Ragan <raganmd@gmail.com>
2021-06-10 21:19:31 -0700

    Control updates

--- dez/dats/python/compositorEXT.py
+++ dez/dats/python/compositorEXT.py
@@ -4,3 +4,20 @@ class controls:
 		self.MyOp = myOp
 		
 		print(f'controls init from {myOp}')
+	
+	def Toggle_glitch(self, value):
+		print(type(value))
+		if value == '1': 
+			ipar.Settings.Glitchfx = value
+		
+		else:
+			ipar.Settings.Toggleglitch = value			
+			delayScript = "setattr(args[0], args[1], args[2])"
+			delayTime = op('filter1').par.width.eval() * me.time.rate
+			run(delayScript, 
+				ipar.Settings,
+				"Glitchfx",
+				int(value), 
+				delayFrames = delayTime)
+			
+		ipar.Settings.Toggleglitch = value
